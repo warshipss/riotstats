ec65b00211ee03996477c5e3f400465f102c49de
Alex Balatsky <alex@skylex.pro>
2020-05-03 07:17:25 +0300

    DEZIIIIIIGN

--- nuxt/components/footer.vue
+++ nuxt/components/footer.vue
@@ -3,15 +3,14 @@
     <div class="container">
       <div class="footer__main">
         <div class="row">
-          <div class="col col-md-6">
+          <div class="col col-md-8">
             <p>
-              {{ $t('This website is not affiliated with Riot Games.') }} <br />
-              {{ $t('Valorant is a registered trademark of Riot Games, Inc.') }} <br />
-              {{ $t('All used images and data is a property of Riot Games, Inc.') }}
+              {{ $t('RiotStats.com isn\'t endorsed by Riot Games and doesn\'t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties.') }} <br />
+              {{ $t('Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.') }}
             </p>
           </div>
 
-          <div class="col col-md-6 footer__twitter">
+          <div class="col col-md-4 footer__twitter">
             <a href="https://twitter.com/riotstats_com" target="_blank">
               <i class="icon icon-twitter" />
             </a>
